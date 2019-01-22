908f216907f80fe111b37f087871296495bbf549
Deepika Ragunathan <deepikaragunathan@gmail.com>
2019-01-21 21:49:41 -0600

    cucumber project using maven

---
+++ CucumberProject/src/main/java/AutomationTesting/CucumberProject/App.java
@@ -0,0 +1,13 @@
+package AutomationTesting.CucumberProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
